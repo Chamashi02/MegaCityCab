4d82a8c1c77d3fa4f33c6a245c516604f082f6a5
Chamashi02 <chamashi.nethsarani20@gmail.com>
2025-03-07 00:52:04 +0530

    UI: book trip UI update

--- src/java/dao/BookingDAO.java
+++ src/java/dao/BookingDAO.java
@@ -1,8 +1,11 @@
+//dao/BookingDAO
 package dao;
 
 import models.Booking;
 import utils.DBConnection;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -29,5 +32,68 @@ public class BookingDAO {
             e.printStackTrace();
             return false;
         }
+        
+    }
+    
+    public static boolean updateBookingStatus(int bookingId, String status) {
+    String query = "UPDATE bookings SET status = ? WHERE booking_id = ?";
+    
+    try (Connection conn = DBConnection.getConnection();
+         PreparedStatement pst = conn.prepareStatement(query)) {
+        
+        pst.setString(1, status);
+        pst.setInt(2, bookingId);
+
+        int rowsUpdated = pst.executeUpdate();
+        return rowsUpdated > 0;
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+        }
+    }
+    
+    public static List<Booking> getAllBookings() {
+        List<Booking> bookings = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DBConnection.getConnection();
+            String query = "SELECT * FROM bookings";
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Booking booking = new Booking(
+                        rs.getInt("user_id"),
+                        rs.getString("cab_type"),
+                        rs.getString("pickup_location"),
+                        rs.getString("dropoff_location"),
+                        rs.getString("pickup_time"),
+                        rs.getString("status"),
+                        rs.getDouble("price"),
+                        rs.getDouble("distance")
+                );
+                bookings.add(booking);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return bookings;
     }
 }
+
+    
+
+
+    
+
